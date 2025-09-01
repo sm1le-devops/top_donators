a8308e7322837d4916a3e049b094c341ef83e237
sm1le-devops <gvh423699@gmail.com>
2025-09-01 11:02:04 +0200

    update profile1

--- routers/auth.py
+++ routers/auth.py
@@ -30,7 +30,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 load_dotenv()
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
-YOUR_DOMAIN = os.getenv("YOUR_DOMAIN", "https://top-donators1.onrender.com")
+YOUR_DOMAIN = os.getenv("YOUR_DOMAIN", "https://top-donators.onrender.com")
 UPLOAD_AVATAR_DIR = "/static/avatars"
 MAX_AVATAR_SIZE = 10 * 1024 * 1024
 os.makedirs(UPLOAD_AVATAR_DIR, exist_ok=True)
